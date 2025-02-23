55dd3ee8e530236b0d2eb2f138cc19011a5fc12f
Dharsan <dharsan.s0503@gmail.com>
2025-02-23 09:42:34 +0530

    tailwind-added

---
+++ Frontend/myapp/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className=' w-full h-full text-red-500 font-bold bg-black-800'>
+            Welcome Sanjay..
+        </div>
+    );
+}
+
+export default HomePage;
