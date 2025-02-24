2f36df5a33e93b7c08bbb6dde20703401822bd15
Sanjay400 <sanjay.gs2020cse@sece.ac.in>
2025-02-24 15:14:13 +0530

    Frist

--- Frontend/myapp/src/App.jsx
+++ Frontend/myapp/src/App.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 import "./App.css"; // Import external CSS
+import LandingPage from "./LandingPage/LandingPage";
 
 function App() {
   return (
-    <div className="main-container">
-      {/* Navbar */}
-      <nav className="navbar">
-        <h1 className="logo">Zomato</h1>
-        <div className="nav-items">
-          <span>Search</span>
-          <span>📍 Coimbatore</span>
-          <button className="nav-btn">Login</button>
-          <button className="nav-btn">Signup</button>
-        </div>
-      </nav>
-
-      {/* Hero Section */}
-      <div className="hero-section">
-        <h1 className="hero-text">Discover the best food & drinks in Your City</h1>
-        <div className="search-bar">
-          <input type="text" placeholder="Enter your location" />
-          <button className="search-btn">Find the Restaurant</button>
-        </div>
-      </div>
+    <div className="App">
+      <LandingPage />
     </div>
   );
 }
