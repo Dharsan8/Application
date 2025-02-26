abd4a6f23d01cce9134b7cc75cf7fffa99ba52a7
Dharsan <dharsan.s0503@gmail.com>
2025-02-26 09:45:32 +0530

    portchanged

--- Frontend/myapp/src/components/Login.jsx
+++ Frontend/myapp/src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
 
-        const response = await fetch("http://localhost:5000/api/login",{
+        const response = await fetch("http://localhost:3000/api/login",{
             method:"POST",
             headers:{
                 "content-type":"application/json"
